4f68a8687da273e991ad995e42d143a9ae60bbd2
Matthew Evans <git@ml-evs.science>
2023-02-22 18:15:27 +0000

    Update script for generating chemical formula database

--- src/chemical_formulae/queries.py
+++ src/chemical_formulae/queries.py
@@ -1,23 +1,33 @@
-"""This script performs queries for high entropy alloy materials to all OPTIMADE APIs."""
+"""This script performs queries for chemical formulae to all OPTIMADE APIs."""
 
 if __name__ == "__main__":
-    import json
-    from pathlib import Path
 
-    import pandas as pd
+    import pymongo as pm
     from optimade.client import OptimadeClient
-    from rich.console import Console
-    from rich.table import Table
 
-    console = Console()
+    client = pm.MongoClient("mongodb://localhost:27017/optimade_example")
+    collection = client.optimade_example.structures
+    collection.create_index("immutable_id", unique=True)
+
+    def insert_into_mongo(url, results):
+        """Inserts data into a MongoDB collection."""
+        prefix = results["meta"].get("provider", {}).get("prefix", None)
+        for entry in results["data"]:
+            formula = entry.pop("attributes")["chemical_formula_reduced"]
+            entry["chemical_formula_reduced"] = formula
+            entry["prefix"] = prefix
+            entry["immutable_id"] = f"{url.scheme}://{url.host}{url.path}/{entry['id']}"
+            try:
+                collection.insert_one(entry)
+            except pm.errors.DuplicateKeyError:
+                continue
 
     download_structures = False
 
     client = OptimadeClient(
-        exclude_providers=["nmd", "aflow"], max_results_per_provider=1000
+        exclude_providers=["nmd"],
+        max_results_per_provider=-1,
+        callbacks=[insert_into_mongo],
     )
 
     all_formulae = client.get(response_fields=["chemical_formula_reduced"])
-
-    with open("formulae.json", "w") as f:
-        json.dump(all_formulae, f)
