24758caa3fc100b37912a4e4b5a29f50e5daaf1b
Matthew Evans <git@ml-evs.science>
2023-06-30 02:10:02 +0100

    Latest draft attempts

--- src/chemical_formulae/embedding.py
+++ src/chemical_formulae/embedding.py
@@ -2,6 +2,7 @@ if __name__ == "__main__":
     import pymongo as pm 
     import pandas as pd
     from chem_wasserstein.ElM2D_ import ElM2D
+    from sklearn.model_selection import train_test_split
 
     db = pm.MongoClient().optimade_example.structures
 
@@ -9,13 +10,9 @@ if __name__ == "__main__":
 
     print(f"# of COD results {db.count_documents(cod_query)}")
 
-    cod = db.find(cod_query)
-
+    cod = db.find(cod_query, limit=1000)
     cod_df = pd.DataFrame(cod)
-
-
-    mapper = ElM2D()
-
-    mapper.fit(cod_df["chemical_formula_reduced"])
-
-    breakpoint()
+    train, test = train_test_split(cod_df, test_size=0.75)
+    mapper = ElM2D(target="cpu", emd_algorithm="wasserstein")
+    mapper.fit_transform(train["chemical_formula_reduced"].values.aslist(), how="UMAP")
+    mapper.plot("umap.html")
